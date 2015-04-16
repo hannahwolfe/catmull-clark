4eaabb2f9283a107e867474780cf50bfeb4d8aed
Hannah Wolfe <hanawolfe@gmail.com>
2015-04-16 16:45:07 -0700

    modified to be a iterative loop

--- catmull.py
+++ catmull.py
@@ -8,7 +8,7 @@ Created on Wed Apr 15 13:11:56 2015
 vertices = [[0,0,0],[1,0,0],[0,1,0],[0,0,1],[1,1,0],[1,0,1],[0,1,1],[1,1,1]]
 sides = [[0,1],[0,2],[0,3],[1,4],[1,5],[2,4],[2,6],[3,5],[3,6],[4,7],[5,7],[6,7]]#verticesIndex
 faces = [[1,2,6,8],[3,4,9,10],[0,2,4,7],[5,6,9,11],[0,1,3,5],[7,8,10,11]]#sidesIndex
-
+iterations = 3
 
 def checkAndAdd (item, array):
     try:
@@ -45,32 +45,38 @@ def force (vertex,vertices):
             vertex[1] + (.2* (vertex[1] - y / (1.0*len(vertices)))),
             vertex[2] + (.2* (vertex[2] - z / (1.0*len(vertices))))]
     
-
-newVertices = vertices[:]
-newSides = []
-newFaces = []
-
-for face in faces:
-    print "face " + str(face)
-    center = centroid(face, vertices, sides)
-    centroidIndex = checkAndAdd(center, newVertices)   
+def iteration(vertices,sides,faces):
+    newVertices = vertices[:]
+    newSides = []
+    newFaces = []
     
-    sideV = [sides[i] for i in face]
-    points = list(set([item for sublist in sideV for item in sublist])) 
-    for p in points:
-        newFace = []
-        for s in sideV:
-            if p in s:
-                midpoint = midPoint(s, newVertices)
-                midPointIndex = checkAndAdd(midpoint, newVertices)
-                newFace.append(checkAndAdd(sorted([p,midPointIndex]), newSides))
-                newFace.append(checkAndAdd(sorted([centroidIndex,midPointIndex]), newSides))
-        checkAndAdd(sorted(newFace), newFaces)
+    for face in faces:
+        print "face " + str(face)
+        center = centroid(face, vertices, sides)
+        centroidIndex = checkAndAdd(center, newVertices)   
+        
+        sideV = [sides[i] for i in face]
+        points = list(set([item for sublist in sideV for item in sublist])) 
+        for p in points:
+            newFace = []
+            for s in sideV:
+                if p in s:
+                    midpoint = midPoint(s, newVertices)
+                    midPointIndex = checkAndAdd(midpoint, newVertices)
+                    newFace.append(checkAndAdd(sorted([p,midPointIndex]), newSides))
+                    newFace.append(checkAndAdd(sorted([centroidIndex,midPointIndex]), newSides))
+            checkAndAdd(sorted(newFace), newFaces)
+        
+    for vertex in vertices:
+        v = newVertices.index(vertex)
+        newVertices[v] = force(vertex, vertices)
+        
+    print newVertices
+    print newSides
+    print newFaces
+    return newVertices, newSides, newFaces
+ 
+   
+for i in range(0,iterations):
+    vertices, sides, faces = iteration(vertices,sides,faces)
     
-for vertex in vertices:
-    v = newVertices.index(vertex)
-    newVertices[v] = force(vertex, vertices)
-
-print newVertices
-print newSides
-print newFaces
